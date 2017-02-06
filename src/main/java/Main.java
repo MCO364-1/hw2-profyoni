8eb5110159347e188e4636eba8df656e34a6edb2
profyoni <jrobinson@touro.edu>
2017-02-06 17:56:50 -0500

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,53 @@
-//package edu.touro.mco364;
 
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-class MyThread extends Thread
-{    // the entry point of a thread is run
+class MyThread extends Thread {    // the entry point of a thread is run
+
+    final int LOOP_MAX = 1000000;
+    int instanceCouter;
+    static int sharedCounter;
+
     @Override
-    public void run(){
-        for(int i=0;i<10;i++)
-        {
-            System.out.println("T2: " + i);
-            Main.threadSleep(100);
+    public void run() {
+        for (int i = 0; i < LOOP_MAX; i++) {
+            instanceCouter++;
+            synchronized("BathroomKey"){ // only one thread may enter at a time
+            MyThread.sharedCounter++;
+            }
+            //System.out.println("T2: " + i);
+            //Main.threadSleep(100);
         }
-    }   
+    }
 
 }
 
-// Write a program that has 10 threads running simlatenously and each will count from 1 to 1000000
-// and add 1 to a  local  variable (no sleeping) and output the total. It should equal 1,000,000
-// Then have them also add 1 to a shared static variable. it should be 10,000,000.
 public class Main {
 
-    public static void threadSleep(int ms){        
+    public static void threadSleep(int ms) {
         try {
             Thread.sleep(ms);
         } catch (InterruptedException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
     public static void main(String[] args) {
-        Thread t = new MyThread();
-        t.start();
-        
-        for(int i=0;i<10;i++)
-        {
-            System.out.println("Main thread: " + i);
-            Main.threadSleep(100);
+
+        ArrayList<MyThread> threadList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            MyThread t = new MyThread();
+            threadList.add(t);
+            t.start();
         }
+        Main.threadSleep(1000);
+        
+        for(MyThread t : threadList){
+            System.out.println(t.instanceCouter + ", " + t.sharedCounter);
+        }
+        
     }
 }
